54a75f4abc3a3d2bd279ac8e170920f9b572aeaf
김 현준 <jkajks123456@einssnc.com>
2021-05-18 18:07:07 +0900

    액션 및 람다식

---
+++ Assets/Scripts/Messenger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Messenger : MonoBehaviour
+{
+    public delegate void Send(string reciever);
+
+    Send onSend;
+
+    void SendMail(string receiver) {
+        Debug.Log("Mail sent to: " + receiver);
+    }
+    void SendMoney(string receiver) {
+        Debug.Log("Money sent to: " + receiver);
+    }
+    private void Start() {
+        onSend += SendMail;
+        onSend += SendMoney;
+        onSend += man => { Debug.Log("Assainate " + man); };
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            onSend("Joon");
+        }
+    }
+}
---
+++ Assets/Scripts/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Worker : MonoBehaviour
+{
+    //delegate void Work();
+    //입력값이 없고, 리턴값이 void인 함수는 너무 많다
+    //그래서 이러한 delegate를 사용하기 위해서
+    Action work;    //Action을 사용한다
+    //==delegate void Action();
+
+    void MoveBricks() {
+        Debug.Log("벽돌을 옮겼다");
+    }
+    void DigIn() {
+        Debug.Log("땅을 팠다");
+    }
+    private void Start() {
+        work += MoveBricks;
+        work += DigIn;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            work();
+        }
+    }
+}
